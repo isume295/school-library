7cd42eb043040507d5eb811081f5be073394bf1a
Sumeya <sumeyaibrahim72@gmail.com>
2023-07-26 20:47:04 +0300

    Create teacher.rb

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require_relative 'person'
+
+class Teacher < Person
+  def initialize(id, age, name, parent_permission, specialization)
+    super(id, age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
