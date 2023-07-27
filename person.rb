e7929038adb5cc6a25fb47976863124e47163c24
Sumeya <sumeyaibrahim72@gmail.com>
2023-07-27 19:57:38 +0300

    Add required changes

--- person.rb
+++ person.rb
@@ -22,9 +22,8 @@ class Person < Nameable
     @name
   end
 
-  def add_rental(rental)
-    @rentals.push(rental)
-    rental.person(self)
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   private
