2baf8b866b744f4670ff394f39c68f533c32458e
Sumeya <sumeyaibrahim72@gmail.com>
2023-07-27 18:18:01 +0300

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,12 +1,21 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
-  def initialize(id, age, name, parent_permission, classroom)
-    super(id, age, name, parent_permission)
-    @classroom = classroom
+  attr_reader :classroom
+
+  def initialize(classroom1, age, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom(classroom1)
+  end
+
+  def classroom=(classroom)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
-    '¯\(ツ)/¯'
+    @age
   end
 end
+
+ 
